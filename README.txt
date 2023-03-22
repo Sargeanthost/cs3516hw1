64a4a7835d2bd359bb66260b7aafb700a7fd7d2c
girraiffe <sargeanthost@gmail.com>
2023-03-22 15:00:08 -0400

    client finished

--- README.txt
+++ README.txt
@@ -10,19 +10,22 @@ RTT on wifi:
         Trial 7: 13
         Trial 8: 10
         Trial 9: 11
+        AVG RTT: 11.4 ms
     example.com:
-        Trial 0:
-        Trial 1:
-        Trial 2:
-        Trial 3:
-        Trial 4:
-        Trial 5:
-        Trial 6:
-        Trial 7:
-        Trial 8:
-        Trial 9:
-        AVG RTT:
-These numbers relative to each other make sense to me, as google.com has a lot more text.
+        Trial 0: 7
+        Trial 1: 7
+        Trial 2: 9
+        Trial 3: 7
+        Trial 4: 8
+        Trial 5: 8
+        Trial 6: 7
+        Trial 7: 8
+        Trial 8: 8
+        Trial 9: 9
+        AVG RTT: 7.8 ms
+These numbers relative to each other make sense to me, as google.com has a lot more text. Also, these numbers match my
+terminal's `ping` command time.
 
-Confirmation of last line:
-    google.com
+Supports both IP and domain name lookup. Many sites have redirects and other weird CDN blocks if you try to access through
+straight IP for some reason, I know my website does this too. But, 52.5.170.204 for komkon.org works well with my program
+and can download through IP or domain.
--- client/http_client.c
+++ client/http_client.c
@@ -4,19 +4,19 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <time.h>
 #include <netinet/tcp.h>
+#include <regex.h>
 
 #define MAX_LINE_LEN 4096
+#define MAX_ARGS 4
 
-//returns the rtt in microseconds
+//Writes a request to the given socket, saves the response to a file, and returns the rtt in microseconds
 uint32_t socketResponse2File(FILE *fp, int sd, char response_buffer[], size_t line_len) {
     struct tcp_info info;
     socklen_t tcp_info_length = sizeof info;
@@ -26,10 +26,13 @@ uint32_t socketResponse2File(FILE *fp, int sd, char response_buffer[], size_t li
     }
 
     if (write(sd, response_buffer, line_len) != line_len) {
-        puts("write to socket did not complete");
+        puts("write to socket did not complete. Please rerun the program");
         exit(1);
     }
-    int ret = getsockopt(sd, SOL_TCP, TCP_INFO, &info, &tcp_info_length);
+    if(getsockopt(sd, SOL_TCP, TCP_INFO, &info, &tcp_info_length) == -1){
+        puts("RTT could not be calculated. Something is wrong with the socket");
+        return 0;
+    }
 
     ssize_t n;
     memset(response_buffer, 0, MAX_LINE_LEN);
@@ -44,9 +47,11 @@ uint32_t socketResponse2File(FILE *fp, int sd, char response_buffer[], size_t li
         exit(1);
     }
     close(sd);
+
     return info.tcpi_rtt;
 }
 
+//converts a domain to an ipv4 ip address
 void domain2ip(char *domain, char *port, char ip_string[]) {
     struct addrinfo hints, *results, *temp;
     int status;
@@ -69,44 +74,71 @@ void domain2ip(char *domain, char *port, char ip_string[]) {
             addr = &(ipv4->sin_addr);
         }
 
-        // convert the IP to a string and print it:
+        // convert the IP to a string
         inet_ntop(temp->ai_family, addr, ip_string, sizeof(char) * INET_ADDRSTRLEN);
     }
 
-    freeaddrinfo(results); // fre
+    freeaddrinfo(results);
+}
+
+//split string in half base on delimiter. delimiter stays with second half
+void split(char *str, char *delimiter, char **first, char **second){
+    char *token = strtok(str, delimiter);
+    *first = token;
+    token = strtok(NULL, delimiter);
+    *second = token;
+}
+
+int is_ip_address(const char *domain) {
+    regex_t regex;
+    //found regex on the internet, tested on https://regexr.com/
+    int ret = regcomp(&regex, "^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}$", REG_EXTENDED);
+    if (ret != 0) {
+        perror("Failed to compile regular expression\n");
+        return 0;
+    }
+
+    int is_ip = 0;
+    ret = regexec(&regex, domain, 0, NULL, 0);
+    if (ret != REG_NOMATCH) {
+        is_ip = 1;
+    }
+
+    regfree(&regex);
+    return is_ip;
 }
 
 int main(int argc, char *argv[]) {
-    char *website_name;
-    char *connection_port;
-    short will_time = 0;
-    //if website_name matchs:
-    // /^(?=.*[^\.]$)((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.?){4}$/
-    //then you have ip address
-//   ./http_client -temp google.com 80
-    if (argc == 3) {
-        website_name = argv[1];
-        connection_port = argv[2];
-    } else if (argc == 4) {
-        website_name = argv[2];
-        connection_port = argv[3];
-        will_time = 1;
-    } else {
+    char *url, *url_domain, *url_path, *port;
+    int will_time = (argc == 4);
+
+    if (argc < 3 || argc > 4) {
         fprintf(stderr, "usage: ./http_client [-temp] server_url port_number\n");
         return 1;
     }
 
+    url = argv[MAX_ARGS - (MAX_ARGS - argc) - 2];
+    port = argv[MAX_ARGS - (MAX_ARGS - argc) - 1];
+
+    split(url, "/", &url_domain, &url_path);
+    url_path = (url_path == NULL) ? "/" : url_path;
+
     char ip_string[INET_ADDRSTRLEN];
+
+    if (is_ip_address(url_domain)) {
+        strcpy(url_domain, ip_string);
+    } else {
+        domain2ip(url_domain, port, ip_string);
+    }
     int socket_descriptor;
 
-    domain2ip(website_name, connection_port, ip_string);
 
     if ((socket_descriptor = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
         puts("socket call error");
         return 3;
     }
     //connect socket to port
-    int temp_connection_port = atoi(connection_port);
+    int temp_connection_port = atoi(port);
     struct sockaddr_in website_sockaddr;
 
     memset(&website_sockaddr, 0, sizeof website_sockaddr);
@@ -116,9 +148,9 @@ int main(int argc, char *argv[]) {
     }
     website_sockaddr.sin_port = htons(temp_connection_port);
     website_sockaddr.sin_family = AF_INET;
-    if(inet_pton(AF_INET,ip_string,&website_sockaddr.sin_addr) < 0){
-        puts("inet_pton");
-        return 99;
+    if (inet_pton(AF_INET, ip_string, &website_sockaddr.sin_addr) < 0) {
+        puts("inet_pton fail");
+        return 10;
     }
 
     if (connect(socket_descriptor, (struct sockaddr *) &website_sockaddr, sizeof(website_sockaddr)) != 0) {
@@ -130,14 +162,17 @@ int main(int argc, char *argv[]) {
     char send_line[MAX_LINE_LEN];
     size_t send_line_len;
     char receive_line[MAX_LINE_LEN];
-    sprintf(send_line, "GET / HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", website_name);
+    sprintf(send_line, "GET %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", url_path, url_domain);
     send_line_len = strlen(send_line);
     FILE *output_file;
-    output_file = fopen("index.html","w");
+    output_file = fopen("index.html", "w");
 
-    printf("Contacting %s...\n", website_name);
-    if(will_time){
-        printf("\nRTT is %u milliseconds\n", socketResponse2File(output_file, socket_descriptor, send_line, send_line_len)/1000);
+    printf("GETTING contents from %s...\n", strcat(url_domain, url_path));
+    if (will_time) {
+        printf("\nRTT is %u milliseconds\n",
+               socketResponse2File(output_file, socket_descriptor, send_line, send_line_len) / 1000);
+    } else {
+        socketResponse2File(output_file, socket_descriptor, send_line, send_line_len);
     }
     return 0;
 }
